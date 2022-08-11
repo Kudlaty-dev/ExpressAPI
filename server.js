81ba94e69605f258c666ea2e119b33a20b7ec9a8
Piotr Kowalski <perek.kowalski@gmail.com>
2022-08-11 13:28:42 +0200

    Router excercise

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
-const express = require ('express');
-const dotenv = require ('dotenv');
+const express = require ('express')
+const app = express()
+const dotenv = require ('dotenv')
+const router = require('./routes/bootcamps')
+app.use(express.json())
 
 //Load env vars
-dotenv.config({path: './config/config.env'});
+dotenv.config({path: './config/config.env'})
 
-const app = express();
+//Launch express app
 
-const PORT = process.env.PORT || 5000;
+
+
+//Route files
+
+
+
+//Mount routers
+app.use('api/v1/bootcamps', router)
+
+
+app.get('/test', (req, res) => {
+    res.status(200).json({success: true, msg: 'Udalo sie'})
+})
+
+
+
+const PORT = process.env.PORT || 5000
 
 app.listen(
     PORT, 
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+)
  
 
 
