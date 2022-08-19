0b908e1a74a0582c25835b5adab953b602bb9ae2
Piotr Kowalski <perek.kowalski@gmail.com>
2022-08-19 22:43:34 +0200

    MongoDB database setup

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const router = require("./routes/bootcamps");
 //const secondrouter = require("./routes/router");
 app.use(express.json());
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 //Dev logging middleware
 //if (process.env.NODE_ENV === "development") {
@@ -13,6 +14,8 @@ app.use(morgan("dev"));
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+connectDB();
 
 //Mount routers
 app.use("/api/v1/bootcamps", router);
@@ -25,7 +28,14 @@ app.get("/test", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close Server & exit process
+  server.close(() => process.exit(1));
+});
