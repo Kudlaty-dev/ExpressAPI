8cb20cc5cb23caa9cb877c2fd7763666d995a016
Piotr Kowalski <perek.kowalski@gmail.com>
2022-08-19 22:59:26 +0200

    Added colors module

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const router = require("./routes/bootcamps");
+const colors = require("colors");
 //const secondrouter = require("./routes/router");
 app.use(express.json());
 const morgan = require("morgan");
@@ -30,12 +31,14 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 //Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   //Close Server & exit process
   server.close(() => process.exit(1));
 });
