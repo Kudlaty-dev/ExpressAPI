03ffb587b9ec3c9d976d4086fde994fb963507c7
Piotr Kowalski <perek.kowalski@gmail.com>
2022-10-09 17:44:41 +0200

    Implemented middleware for advanced result search

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -9,78 +9,7 @@ const fileupload = require("express-fileupload");
 //@route    GET /api/v1/bootcamps
 //@access   Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  let query;
-  //Copy req.query
-  const reqQuery = { ...req.query };
-
-  //Fields to exclude from query (like 'select' etc.)
-
-  const removeFields = ["select", "sort", "limit", "page"];
-
-  //Loop over removeFields array and remove matches from reqQuery
-  removeFields.forEach((param) => delete reqQuery[param]);
-
-  //Create query string from the query
-  let queryStr = JSON.stringify(reqQuery);
-
-  //Create operators ($gt, $gte, $lt, $lte, $in) based on query string
-  //1. search and replace through the string for keywords and replace them with $<keyword>, so that it works in express.
-  queryStr = queryStr.replace(
-    /\b(gt|gte|lt|lte|in)\b/g,
-    (match) => `$${match}`
-  );
-
-  //Finding resource
-  query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
-
-  //Select Fields
-  if (req.query.select) {
-    const fields = req.query.select.split(",").join(" ");
-    query = query.select(fields);
-  }
-
-  //Sort
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(",").join(" ");
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort("-createdAt");
-  }
-
-  //Pagination
-  const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 1;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const total = await Bootcamp.countDocuments();
-
-  query = query.skip(startIndex).limit(limit);
-
-  //Execute query
-  const bootcamps = await query;
-
-  //Pagination result
-  const pagination = {};
-  if (endIndex < total) {
-    pagination.next = {
-      page: page + 1,
-      limit,
-    };
-  }
-
-  if (startIndex > 0) {
-    pagination.prev = {
-      page: page - 1,
-      limit,
-    };
-  }
-
-  res.status(200).json({
-    success: true,
-    count: bootcamps.length,
-    pagination,
-    data: bootcamps,
-  });
+  res.status(200).json(res.advancedResults);
 });
 
 //@desc     Get single bootcamp
