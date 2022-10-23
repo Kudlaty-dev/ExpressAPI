19e22d7a5375ec3d27523426dae578cb86e2e69b
Piotr Kowalski <perek.kowalski@gmail.com>
2022-10-23 22:52:13 +0200

    Get single review route added. DB seeder updated - reviews data is included

--- seeder.js
+++ seeder.js
@@ -13,6 +13,7 @@ const Bootcamp = require("./models/Bootcamp");
 const Courses = require("./models/Course");
 const Course = require("./models/Course");
 const User = require("./models/User");
+const Review = require("./models/Review");
 
 //Connecto to DB
 mongoose.connect(mongoUri, {
@@ -32,6 +33,9 @@ const courses = JSON.parse(
 const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
 );
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, "utf-8")
+);
 
 //Import into DB
 const importData = async () => {
@@ -39,6 +43,7 @@ const importData = async () => {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
     console.log("Data Imported...".green.inverse);
     process.exit();
   } catch (err) {
@@ -52,6 +57,7 @@ const deleteData = async () => {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
     console.log("Data destroyed...".red.inverse);
     process.exit();
   } catch (err) {
