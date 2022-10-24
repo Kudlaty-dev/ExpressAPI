50e934476ae59d23104fb3f517eb0e00ede42537
Piotr Kowalski <perek.kowalski@gmail.com>
2022-10-25 00:40:22 +0200

    Added rate limiting, HPP and CORS

--- server.js
+++ server.js
@@ -11,9 +11,14 @@ const reviews = require("./routes/reviews");
 
 const fileupload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xssClean = require("xss-clean");
+const expressRateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+
 const colors = require("colors");
 const errorHandler = require("./middleware/error");
 
@@ -47,6 +52,19 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xssClean());
+
+// Rate limiting
+const limiter = expressRateLimit({
+  windowsMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+app.use(limiter);
+
+//Prevent http param pollution
+app.use(hpp());
+
+//Enable CORS requests
+app.use(cors());
 
 //Set static folder
 //Static public directory
