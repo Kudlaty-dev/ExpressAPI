38e3871efc43ba25c9333b00eff2728842c12e68
Piotr Kowalski <perek.kowalski@gmail.com>
2022-10-24 18:33:08 +0200

    DELETE route added for reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -85,7 +85,7 @@ exports.updateReview = asyncHandler(async (req, res, next) => {
     );
   }
 
-  // Make sure user is course owner
+  // Make sure user is review owner
   if (review.user.toString() !== req.user.id && req.user.role !== "admin") {
     return next(
       new ErrorResponse(
@@ -99,4 +99,31 @@ exports.updateReview = asyncHandler(async (req, res, next) => {
     success: true,
     data: review,
   });
+});
+
+//@desc     Delete a single review
+//@route    DELETE /api/v1/reviews/:id
+//@access   Private
+
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`.blue, 404)
+    );
+  }
+
+  // Make sure user is review owner
+  if (review.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to delete review ${review._id}`,
+        401
+      )
+    );
+  }
+
+  await review.remove();
+  res.status(200).json({ success: true, data: review });
 });
